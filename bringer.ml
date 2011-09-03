898ed33794d09678d9bdb385960942b8ee698203
carsten <devnull@localhost>
2011-09-03 19:25:28 +0200

    Todos aktualisiert.

--- bringer.ml
+++ bringer.ml
@@ -1,8 +1,17 @@
-(* #!/usr/bin/lablgtk2 todos: zum thema unit tests: durch die intensive    *)
-(* eigennutzung ist viel abgedeckt; was fehlt, ist (1) der test anderer    *)
-(* systemkonfigurationen (fehlendes wmctrl, andere bildschirmgröße,        *)
-(* dateirechte, etc., und (2) nicht ganz so offensichtliche merkmale wie   *)
-(* die sortierung der desktops und der history github + co? gtk?           *)
+(* #!/usr/bin/lablgtk2 *)
+
+(* todos: *)
+
+(* zum thema unit tests: durch die intensive eigennutzung ist viel         *)
+(* abgedeckt; was fehlt, ist (1) der test anderer systemkonfigurationen    *)
+(* (fehlendes wmctrl, dateirechte, etc., und (2) nicht ganz so             *)
+(* offensichtliche merkmale wie die sortierung der desktops und der        *)
+(* history                                                                 *)
+
+(* github + co? gtk? *)
+
+(* description_of_window über eine Datei mit Kommando-Namen                *)
+(* implementieren. Nur: "Was ist die Regel, und was ist die Ausnahme?"     *)
 
 module String = ExtLib.String
 module Hashtbl = ExtLib.Hashtbl
